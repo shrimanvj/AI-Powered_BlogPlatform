3599e680840f304dc2b9853c542875578b2069ff
shrimanvj <shrimanvijay06@gmail.com>
2025-07-11 04:43:50 +0530

    Files Added

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState } from "react";
+import axios from 'axios';
+import toast from "react-hot-toast";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
+console.log('Base URL:', baseURL);
+axios.defaults.baseURL = baseURL;
+
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children })=>{
+
+    const navigate = useNavigate()
+
+    const [token, setToken] = useState(null)
+    const [blogs, setBlogs] = useState([])
+    const [input, setInput] = useState("")
+
+
+    const fetchBlogs = async ()=>{
+        try {
+            const { data } = await axios.get('/api/blog/all');
+            if(data.success){
+                setBlogs(data.blogs)
+            } else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+    useEffect(()=>{
+        fetchBlogs();
+        const token = localStorage.getItem('token')
+        if(token){
+            setToken(token);
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        }
+    },[])
+
+     const value = {
+        axios, navigate, token, setToken, blogs, setBlogs, input, setInput, fetchBlogs
+     }
+
+    return (
+
+        <AppContext.Provider value={value}>
+            { children }
+        </AppContext.Provider>
+    )
+}
+
+
+export const useAppContext = ()=>{
+    return useContext(AppContext)
+};
